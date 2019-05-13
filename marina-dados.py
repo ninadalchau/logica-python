a76a3589aa0e50a8ba5c7d6b137f315e9b5aa0be
ninadalchau <50593992+ninadalchau@users.noreply.github.com>
2019-05-13 15:24:15 -0300

    Add files via upload

---
+++ marina-dados.py
@@ -0,0 +1,13 @@
+from random import randint
+
+print("Jodo de Dado:")
+
+dados1 = randint(1, 6)
+dados2 = randint(1, 6)
+dados3 = randint(1, 6)
+dados4 = randint(1, 6)
+
+print("Dado 1:", dados1)
+print("Dado 2:", dados2)
+print("Dado 3:", dados3)
+print("Dado 4:", dados4)
